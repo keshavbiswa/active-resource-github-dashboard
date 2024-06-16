89aff77fcf08976a07de236ec2193eabf3dc4270
Keshav Biswa <keshavbiswa21@gmail.com>
2024-06-16 18:08:14 +0530

    Added logging

---
+++ config/initializers/active_resource_logging.rb
@@ -0,0 +1,8 @@
+ActiveSupport::Notifications.subscribe('request.active_resource') do |name, start, finish, id, payload|
+  uri = payload[:request_uri]
+  method = payload[:method]
+  result = payload[:result]
+  duration = (finish - start) * 1000  # Convert duration to milliseconds
+
+  Rails.logger.debug("[ActiveResource] #{method.upcase} #{uri} completed in #{duration.round(2)}ms with result: #{result.code}")
+end

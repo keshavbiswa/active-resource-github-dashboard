8ac53cad9d14f9ddb7164ec637c2cf21b9a3fbd1
Keshav Biswa <keshavbiswa21@gmail.com>
2024-06-16 18:35:10 +0530

    Added repositories

--- app/resources/github/user.rb
+++ app/resources/github/user.rb
@@ -7,6 +7,10 @@ class Github::User < Github::Base
     Github::Organization.find(:all, from: '/user/orgs')
   end
 
+  def repositories
+    Github::Repository.find(:all, from: '/user/repos', params: { per_page: 100, sort: 'updated', type: 'public' })
+  end
+
   def self.find_authenticated
     find(:one, from: '/user')
   end

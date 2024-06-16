54a66c85881e5985e4706796c2696525d992f9b0
Keshav Biswa <keshavbiswa21@gmail.com>
2024-06-16 17:02:06 +0530

    replaced @organizations with @github_user.organizations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @organizations = Github::Organization.all
+    # @organizations = Github::Organization.all
     @github_user = Github::User.find_authenticated
   end
 end
--- app/resources/github/user.rb
+++ app/resources/github/user.rb
@@ -1,6 +1,8 @@
 class Github::User < Github::Base
   self.element_name = 'user'
 
+  has_many :organizations, class_name: 'Github::Organization'
+
   def self.find_authenticated
     find(:one, from: '/user')
   end
